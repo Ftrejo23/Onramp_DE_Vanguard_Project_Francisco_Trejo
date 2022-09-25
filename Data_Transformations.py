5edc44e22ebb8b25a6bf1519020e493ce5781209
ftrejo24 <ftrejo9717@gmail.com>
2022-09-25 16:31:45 -0700

    starting data type check

--- Data_Transformations.py
+++ Data_Transformations.py
@@ -9,7 +9,7 @@ track_feat = pd.read_csv('./data/track_feat.csv')
 
 # create function to check if there are duplicate row values in the dfs
 def check_for_dups(df, table):
-    '''Function takes in a pandas dataframe (df) and the name if the df as a string (table)'''
+    '''Function takes in a pandas dataframe (df) and the name of the df as a string (table)'''
     potential_dups = []
     # iterate through the columns in the df
     for col in df.columns:
@@ -25,6 +25,8 @@ check_for_dups(artist, 'artist')
 check_for_dups(album, 'album')
 check_for_dups(track, 'track')
 check_for_dups(track_feat, 'track_feat')
+
+print('Removing duplicate values from data...')
 
 # further EDA was done in jupyter notebook, see readme for more info
 # drop duplicate albums with the same name
@@ -43,3 +45,17 @@ final_track_ids = track['track_id'].tolist()
 track_feat = track_feat[track_feat['track_id'].isin(final_track_ids)]
 # drop duplicate tracks with the same track_id
 track_feat.drop_duplicates(subset=['track_id'], inplace=True)
+
+print('Duplicate values successfully removed!')
+
+# function to quickly check if there are null values in the dataframes
+def check_for_nulls(dfs, tables):
+    '''Function takes in a list of pandas dataframe (dfs) and the name of the df as a list (tables)'''
+    for df, table in zip(dfs, tables):
+        if df.isnull().sum().sum() == 0:
+            print(f'No null values in {table} dataframe!')
+        else:
+            print(f'{tables} has {df.isnull().sum().sum()} null value(s), further investigate.')
+
+print('Checking data for null values...')
+check_for_nulls([artist, album, track, track_feat], ['artist', 'album', 'track', 'track_feat'])
